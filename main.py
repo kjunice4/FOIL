583dd64153f794172405bed41b482ca7d980c497
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-03-04 15:58:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,21 +28,21 @@ Builder.load_string("""
             font_size: 50
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
-            height: 100
-            text: "FOIL Method"
+            height: 200
+            text: "Tap anywhere to continue"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left"         
+                
         Button:
             font_size: 50
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
-            height: 100
-            text: "KSquared-math,LLC ©"
+            height: 200
+            text: "KSquared-math,LLC © : FOIL Method"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
-        
 """)
 
 # Menu
