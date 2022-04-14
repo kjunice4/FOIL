779b6d5cce3031c51e55bb78acbc547437a6a3c1
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-04-14 10:21:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ Builder.load_string("""
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
             height: 200
-            text: "KSquared-math,LLC © : FOIL Method Calculator"
+            text: "KSquared-Mathematics : FOIL Method Calculator"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
@@ -86,10 +86,11 @@ Builder.load_string("""
                 font_size: 75
                 size_hint_y: None
                 height: 200
-                text: "Visit KSquared,LLC"
+                padding: 10, 10
+                text: "Visit KSquared-Mathematics"
                 on_release:
                     import webbrowser
-                    webbrowser.open('https://kevinjunice.wixsite.com/ksquaredllc')
+                    webbrowser.open('https://www.ksquaredmathematics.com/subscribe') 
             
             Button:
                 font_size: 75
@@ -107,10 +108,10 @@ Builder.load_string("""
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
-                text: "Share KSquared-math,LLC ©"
+                text: "Share KSquared-Mathematics ©"
                     
             Image:
-                source: 'KSquared_QR_code.png'
+                source: 'KSquared_QR.png'
                 size_hint_y: None
                 height: 1000
                 width: 1000
